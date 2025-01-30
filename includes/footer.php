f5cda73f4c331d77bfa43af8722c253372969af1
Sadi94 <sadimohammad65@gmail.com>
2025-01-30 21:34:27 +0600

    ems

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+
+    <div class="footer bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="copyright">
+                <p>
+                || © Event Management System 2025 || All Rights Reserved. Designed by <a href="https://github.com/Sadi94" target="_blank">Sadi </a> ||
+                </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
