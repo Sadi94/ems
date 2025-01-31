ec2c19ff9b91ab8c375647c7d42328aa84836a3f
Sadi94 <sadimohammad65@gmail.com>
2025-01-31 21:51:35 +0600

    gcp connection

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,10 +1,11 @@
 <?php
-//time zone
-date_default_timezone_set('Asia/Dhaka');
-//database connection
-$con=mysqli_connect("localhost","root","","bbs");
-if(mysqli_connect_errno()){
-echo "Connection Fail".mysqli_connect_error();
-}
+  $url = parse_url(getenv("mysql://root:adminems@34.172.189.141:3306/ems?reconnect=true"));
+  //time zone
+  date_default_timezone_set('Asia/Dhaka');
+  //database connection
+  $con=mysqli_connect("34.172.189.141:3306","root","adminems","ems");
+  if(mysqli_connect_errno()){
+    echo "Connection Fail".mysqli_connect_error();
+  }
 
-  ?>
+?>
